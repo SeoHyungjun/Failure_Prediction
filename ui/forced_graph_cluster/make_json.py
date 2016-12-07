dbefb23f5628b20429f0ce4ebb0edb6438056071
youngwoo <koain@naver.com>
2016-12-07 12:55:40 +0900

    make_json.py add

--- ui/forced_graph_cluster/make_json.py
+++ ui/forced_graph_cluster/make_json.py
@@ -1,39 +1,59 @@
 #!/bin/python
 
 import json
+import random
 
 nodes = list()
 links = list()
-node_num = 1000
+groups = list()
 rack_num = 1
+sum_nodes = 0
+
+group_num = 15
 
 
-for i in range(0, node_num) :
-	if ( (i != 0) and (i % 10) == 0 ) :
-		rack_num = rack_num + 1
+# create node(new) and linking node(except cluster linking) 
+for rack_num in range(0, group_num) :
+	node_num_in_group = random.randrange(6, 20) # determine node_num in this group
+	groups.append({'group_num' : rack_num, 'start_node' : sum_nodes, 'node_num' : node_num_in_group,'graphed' : 0})
 
-	name = 'Server-' + str(rack_num) + '-'  + str((i%10) + 1)
-	node = {'name' : name, 'group' : rack_num, 'status' : '0'}
-	nodes.append(node)
+	for node_num in range (0, node_num_in_group) : # create node and linking in group
+		name = 'Server-' + str(rack_num) + '-' + str(node_num+1)
+		node = {'name' : name, 'group' : rack_num+1, 'status' : '0'}
+		nodes.append(node)
 
-
-node_num_in_group = 10
-group_num = 100
-
-# linking in group
-for i in range(0, group_num) :
-
-	if( i != group_num-1) :
-		source = i*node_num_in_group
-		target = source + node_num_in_group
+		source = sum_nodes
+		target = sum_nodes + node_num
 		link = {'source' : source, 'target' : target}
 		links.append(link)
 
-	for j in range(0, node_num_in_group) :
-		source = i * node_num_in_group
-		target = source + j
-		link = {'source' : source, 'target' : target}
-		links.append(link)
+	sum_nodes = sum_nodes + node_num_in_group
+
+# cluster graph linking
+
+sum_nodes = 0
+neighbors = 0
+groups_in_network = 0
+for rack_num in range (0, group_num) :
+	if groups_in_network == group_num :
+		break
+
+	if neighbors == 0 :
+		neighbors = random.randrange(3,7)
+		source = groups[rack_num]['start_node']
+		groups_in_network += 1
+
+
+	while True : #find to connect clustered graph
+		select_group = random.randrange(0, group_num)
+		if(groups[select_group]['graphed'] == 0) :
+			groups[select_group]['graphed'] = 1
+			target = groups[select_group]['start_node']
+			link = {'source' : source, 'target' : target}
+			links.append(link)
+			neighbors = neighbors - 1
+			groups_in_network += 1
+			break
 
 data = {'nodes' : nodes, 'links' : links}
 
