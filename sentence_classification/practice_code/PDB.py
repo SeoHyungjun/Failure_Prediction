da36c555de307190f8094987e3426a86ccc491b3
Choi <superbag2010@naver.com>
2016-12-15 15:51:20 +0900

    clean up

--- sentence_classification/practice_code/PDB.py
+++
@@ -1,9 +0,0 @@
-import pdb
-
-a = "a string"
-
-print ("Before set_trace()")
-pdb.set_trace()
-
-print ("After1 set_trace()")
-print ("After2 set_trace()")
--- sentence_classification/practice_code/parse.py
+++
@@ -1,8 +0,0 @@
-import re
-
-[a, b] = [[[3,3,3], [x,x]] for x in range(2)]
-c = [a, b]
-print ("a = {}".format(a))
-print ("b = {}".format(b))
-print ("c[1] = {}".format(c[1]))
-
