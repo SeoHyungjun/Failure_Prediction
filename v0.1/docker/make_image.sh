2e7efddd30f2a48f2e93e2aad90e65e5ea9804a0
syseng <syseng@Docker.Syseng>
2017-03-05 14:55:43 +0900

    copy .ssh directory if present directory is not /home/syseng/docker

--- v0.1/docker/make_image.sh
+++ v0.1/docker/make_image.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-cp Dockerfile ~
-cp make_image.sh ~
-cd ~
+PWD="$(pwd)"
+
+if [ "$PWD" != "$HOME" ]
+then
+  echo "copy '~/.ssh'directory in this directory"
+  cp -r ~/.ssh .
+fi
+
 docker build -t fp:dev_env .
-rm -f Dockerfile
-rm -f make_image.sh
+
+if [ "$PWD" != "$HOME" ]
+then
+  echo "remove '.ssh' directory in this directory"
+  rm -rf .ssh
+fi
+
 docker run -it -p 6006:6006 --name FP_final fp:dev_env
