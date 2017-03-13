50dbc806e8c27d5c08788ce0634e5f03c63998c9
youngwoo <koain@naver.com>
2017-03-13 15:03:55 +0900

    added fucntion config and print_config in ML_procces_class
    added proto type of config file

--- v0.1/ml_process/trainer/trainer.py
+++ v0.1/ml_process/trainer/trainer.py
@@ -4,30 +4,18 @@ import sys
 sys.path.insert(0, '..')
 import ml_process
 
-class trainer(ml_process.ml_process_main) :
-	def __init__(self):
-		super.__init__()
-		config()
+class Trainer(ml_process.ML_process_class) :
+    def __init__(self):
+        super().__init__()
 
-	def config(self, config_fname='config'):
-		super().config()
+    def read_data(self, data_read_where):
+        pass
 
-		config_parse(config_fname)
-
-	def config_parse(self, config_fname):
-		f=open(config_fname, 'r')
-
-#		.... something parsing
-
-		self.algo_num=1
-		self.model_list.append('cnn')
-		self.transform_list.append('transpose')
-		self.data_read_where='file' # or 'db' or 'pipe' or 'msg q'
-
-	def read_data(self, data_read_where):
-		pass
-
-	def main(self):
-		self.read_data(
+    def main(self):
+        self.config()
 
 
+if __name__ == '__main__' :
+    train = Trainer()
+    train.main()
+    train.print_config()
