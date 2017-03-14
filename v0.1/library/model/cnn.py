cb7299093ff41b08a94e4f22a2ed1b057d6f8557
choi <superbag2010@naver.com>
2017-03-14 10:37:06 +0000

    init CNN model

--- v0.1/library/model/cnn.py
+++ v0.1/library/model/cnn.py
@@ -19,6 +19,8 @@ class CNN(Model):
     # numb_filters : the number of each size of filter
     # regularization : dropout_keep_prob, l2_reg_lambda(when not applied, each value are 1.0, 0.0)
 
+    # pooling_size, dropout(Conv, NN), activation func, variable initializer
+
         # Placeholders for input, output and dropout
         self.input_x = tf.placeholder(tf.float32, [None, input_size[0], input_size[1]], name="input_x")
         self.expanded_input_x = tf.expand_dims(self.input_x, -1)
@@ -43,18 +45,66 @@ class CNN(Model):
                     padding="VALID",    # no padding
                     name="conv")
                 # Apply nonlinearity
-                h = tf.nn.relu(tf.nn.bias_add(conv, b), name="relu")
-        
+                conv_relu = tf.nn.relu(tf.nn.bias_add(conv, b), name="relu")
+       
                 # Maxpooling over the outputs
                 pooled = tf.nn.max_pool(
-                    h,
+                    conv_relu,
                     ksize=[1, input_size[0] - filter_size[0] + 1, input_size[1] - filter_size[1] + 1, 1],
                     strides=[1,1,1,1],
                     padding="VALID",
                     name="pool")
                 pooled_outputs.append(pooled)
         
-        self.aaa = pooled_outputs
+        # Combine all the pooled featrues
+        num_filters_total = num_filters * len(filter_sizes)
+        pooled_concat = tf.concat(pooled_outputs, 3)
+        pooled_flat = tf.reshape(pooled_concat, [-1, num_filters_total])
+
+        with tf.name_scope("conv-dropout"):
+            conv_drop = tf.nn.dropout(pooled_flat, dropout_keep_prob)
+        
+        
+        # Hidden_NN layer
+        pre_num_node = num_filters_total
+        NN_result = [None] * (len(num_NN_nodes) + 1)
+        NN_result[0] = conv_drop
+        for index, num_node in enumerate(num_NN_nodes):
+            if num_node == 0:
+                index = -1
+                break
+            with tf.name_scope("completely_connected_NN_layer{}".format(index+1)):
+                W = tf.get_variable(
+                    "W_layer{}".format(index+1),
+                    shape = [pre_num_node, num_node],
+                    initializer = tf.contrib.layers.xavier_initializer())
+                b = tf.Variable(tf.constant(0.1, shape=[num_node]), name = "b")
+                l2_loss += tf.nn.l2_loss(W)
+                l2_loss += tf.nn.l2_loss(b)
+                NN_result[index+1] = tf.sigmoid(tf.nn.xw_plus_b(NN_result[index], W, b, name="NN_result{}".format(index+1)))
+                with tf.name_scope("dropout"):
+                    NN_result[index+1] = tf.nn.dropout(NN_result[index+1], dropout_keep_prob)
+                pre_num_node = num_node
+
+
+        with tf.name_scope("output_layer"):
+            W = tf.get_variable(
+                "W",
+                shape=[pre_num_node, num_output],
+                initializer=tf.contrib.layers.xavier_initializer())
+            b = tf.Variable(tf.constant(0.1, shape=[num_output]), name="b")
+            l2_loss += tf.nn.l2_loss(W)
+            l2_loss += tf.nn.l2_loss(b)
+
+            self.scores = tf.nn.xw_plus_b(NN_result[index+1], W, b, name="scores")
+            
+
+        with tf.name_scope("eval_info"):
+            subtraction = tf.subtract(self.scores, self.input_y)
+            equal = tf.equal(self.scores, self.input_y)
+
+
+        self.test = conv_drop
     
 
     def restore(self):
