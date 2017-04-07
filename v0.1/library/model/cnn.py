057e887bb6582753e0d6cd29475b43d59c66a8c2
choi <superbag2010@naver.com>
2017-04-07 13:28:49 +0000

    adding re-training

--- v0.1/library/model/cnn.py
+++ v0.1/library/model/cnn.py
@@ -13,25 +13,23 @@ class CNN(Model):
         pass
 
 
-    def create_model(self, data_file_path, input_height, num_NN_nodes, num_output, filter_sizes, num_filters, dropout_keep_prob=1.0, l2_reg_lambda=0.0):
+    def create_model(self, x_height, x_len, num_NN_nodes, y_size, filter_sizes, num_filters, dropout_keep_prob=1.0, l2_reg_lambda=0.0):
     # Model parameter of create_model
-    # input_height : height of input matrix
+    # x_height : height of input matrix
     # input_size : size of input matrix(two-dimention), [height, width]  e.g. [3,4]
     # num_NN_nodes : fully connected NN nodes(array)  e.g. [3,4,5,2]
-    # num_output : the number of output nodes. if regression, num_output = 1.
+    # y_size : the number of output nodes. if regression, y_size = 1.
     # filter_sizes : list of size of filter matrix(two-dimention)  e.g. [[1,2], [2,3], ...]
     # numb_filters : the number of each size of filter
     # regularization : dropout_keep_prob, l2_reg_lambda(when not applied, each value are 1.0, 0.0)
 
     # pooling_size, dropout(Conv, NN), activation func, variable initializer
 
-        # Load input data
-        self.x, x_len, self.y = make_input.split_xy(csv_file_path=data_file_path, x_height=input_height, y_size=num_output)
 
         # Placeholders for input, output and dropout
-        self.input_x = tf.placeholder(tf.float32, [None, input_height, x_len], name="input_x")
+        self.input_x = tf.placeholder(tf.float32, [None, x_height, x_len], name="input_x")
         self.expanded_input_x = tf.expand_dims(self.input_x, -1)
-        self.input_y = tf.placeholder(tf.int32, [None, num_output], name="input_y" )
+        self.input_y = tf.placeholder(tf.int32, [None, y_size], name="input_y" )
         self.dropout_keep_prob = tf.placeholder(tf.float32, name="dropout_keep_prob") 
        
         # Keeping track of 12 regularization loss (optional)
@@ -57,7 +55,7 @@ class CNN(Model):
                 # Maxpooling over the outputs
                 pooled = tf.nn.max_pool(
                     conv_relu,
-                    ksize=[1, input_height - filter_size[0] + 1, x_len - filter_size[1] + 1, 1],
+                    ksize=[1, x_height - filter_size[0] + 1, x_len - filter_size[1] + 1, 1],
                     strides=[1,1,1,1],
                     padding="VALID",
                     name="pool")
@@ -97,9 +95,9 @@ class CNN(Model):
         with tf.name_scope("output_layer"):
             W = tf.get_variable(
                 "W",
-                shape=[pre_num_node, num_output],
+                shape=[pre_num_node, y_size],
                 initializer=tf.contrib.layers.xavier_initializer())
-            b = tf.Variable(tf.constant(0.1, shape=[num_output]), name="b")
+            b = tf.Variable(tf.constant(0.1, shape=[y_size]), name="b")
             l2_loss += tf.nn.l2_loss(W)
             l2_loss += tf.nn.l2_loss(b)
 
@@ -110,7 +108,7 @@ class CNN(Model):
 
         with tf.name_scope("loss"):
             losses = tf.nn.softmax_cross_entropy_with_logits(logits=self.scores, labels=self.input_y)
-            self.loss = tf.reduce_mean(losses) + (l2_reg_lambda * l2_loss)
+            self.loss = tf.add(tf.reduce_mean(losses), (l2_reg_lambda * l2_loss), name="A")
             tf.summary.scalar("loss_summary", self.loss)
 
         with tf.name_scope("eval_info"):
@@ -119,23 +117,24 @@ class CNN(Model):
             tf.summary.scalar("accuracy_summary", accuracy)
 
 
-    def restore(self, graph, model_name):
-        model_dir = os.path.join(ct.STR_DERECTORY_ROOT, model_name, ct.STR_DERECTORY_GRAPH, "model")
+    def restore(self, sess, graph, model_name):
+        checkpoint_file_path = os.path.join(ct.STR_DERECTORY_ROOT, model_name, ct.STR_DERECTORY_GRAPH)
 
-        checkpoint_file = tf.train.latest_checkpoint(model_dir)
-        restorer = tf.train.import_meta_graph("{}-300.meta".format(model_dir))
-        with tf.Session() as sess:
-            restorer.restore(sess, model_dir)
+        checkpoint_file = tf.train.latest_checkpoint(checkpoint_file_path)
+        restorer = tf.train.import_meta_graph("{}.meta".format(checkpoint_file))
+        restorer.restore(sess, "{}".format(checkpoint_file))
+
         self.input_x = graph.get_operation_by_name("input_x").outputs[0]
         self.input_y = graph.get_operation_by_name("input_y").outputs[0]
-    
+        self.dropout_keep_prob = graph.get_operation_by_name("dropout_keep_prob").outputs[0]
+        self.loss = graph.get_operation_by_name("A").outputs[0]
       
     def _eval(self):
         print ("Eval model trained!!!")
         pass
 
 
-    def train(self, dev_sample_percentage, model_name, batch_size, num_epochs, evaluate_every, saver_every, dropout_keep_prob=0.5, out_subdir=ct.STR_DERECTORY_ROOT):
+    def train(self, x, y, sess, dev_sample_percentage, model_name, batch_size, num_epochs, evaluate_every, saver_every, dropout_keep_prob=0.5, out_subdir=ct.STR_DERECTORY_ROOT):
     ### train parameter ###
     # dev_sample_percentage : percentage of the training data to use for validation"
     # data_file_path : Data source for training
@@ -155,51 +154,43 @@ class CNN(Model):
         summary_dir = os.path.join(ct.STR_DERECTORY_ROOT, model_name, ct.STR_DERECTORY_SUMMARY) 
         summary_train_dir = os.path.join(summary_dir, ct.STR_DERECTORY_SUMMARY_TRAIN) 
         summary_dev_dir = os.path.join(summary_dir, ct.STR_DERECTORY_SUMMARY_DEV)
-        model_dir = os.path.join(ct.STR_DERECTORY_ROOT, model_name, ct.STR_DERECTORY_GRAPH, "model")
+        model_dir = os.path.join(ct.STR_DERECTORY_ROOT, model_name, ct.STR_DERECTORY_GRAPH)
+        model_prefix = os.path.join(model_dir, ct.STR_SAVED_MODEL_PREFIX)
 
         # make training/validation data batch by batch
-        x_train, x_val, y_train, y_val = make_input.divide_fold(self.x, self.y, num_fold=10)
+        x_train, x_val, y_train, y_val = make_input.divide_fold(x, y, num_fold=10)
         batches = make_input.batch_iter(x_train, y_train, batch_size, num_epochs)
-
-        # setting session for training 
-        session_conf = tf.ConfigProto(
-            allow_soft_placement=True,
-            log_device_placement=False)
 
 
         # Training
-        with tf.Session(config=session_conf) as sess:
-            # 1. Define Training procedure
-            global_step = tf.Variable(0, name="global_step", trainable=False)
-            optimizer = tf.train.AdamOptimizer(1e-3)
-            grads_and_vars = optimizer.compute_gradients(self.loss)
-            train_op = optimizer.apply_gradients(grads_and_vars, global_step=global_step)
+        # 1. Define Training procedure
+        global_step = tf.Variable(0, name="global_step", trainable=False)
+        optimizer = tf.train.AdamOptimizer(1e-3)
+        grads_and_vars = optimizer.compute_gradients(self.loss)
+        train_op = optimizer.apply_gradients(grads_and_vars, global_step=global_step)
 
-            # 2. setting summary writer(tensorboard) and saver(save learned graph) operation
-            summary_op = tf.summary.merge_all()
-            train_writer = tf.summary.FileWriter(summary_train_dir, sess.graph)
-
-            model_saver = tf.train.Saver(tf.global_variables())
-             
-
-            # 3. do training
-            sess.run(tf.global_variables_initializer())
-#            tf.train.export_meta_graph(model_dir)
-            for batch in batches:
-                x_batch = batch[0]
-                y_batch = batch[1]
-                feed_dict = {
-                    self.input_x : x_batch,
-                    self.input_y : y_batch,
-                    self.dropout_keep_prob : dropout_keep_prob
-                }
-
-                _, step, summary = sess.run(
-                    [train_op, global_step, summary_op], feed_dict)
-                train_writer.add_summary(summary, step)
-                if step % saver_every == 0:
-                    model_saver.save(sess, model_dir, global_step=step)
-                    print("Save leanred graph at step {}".format(step))
+        # 2. setting summary writer(tensorboard) and saver(save learned graph) operation
+        summary_op = tf.summary.merge_all()
+        train_writer = tf.summary.FileWriter(summary_train_dir, sess.graph)
+        model_saver = tf.train.Saver(tf.global_variables())
+         
+        # 3. do training
+        sess.run(tf.global_variables_initializer())
+#        tf.train.export_meta_graph(model_prefix)
+        for batch in batches:
+            x_batch = batch[0]
+            y_batch = batch[1]
+            feed_dict = {
+                self.input_x : x_batch,
+                self.input_y : y_batch,
+                self.dropout_keep_prob : dropout_keep_prob
+            }
+            _, step, summary = sess.run(
+                [train_op, global_step, summary_op], feed_dict)
+            train_writer.add_summary(summary, step)
+            if step % saver_every == 0:
+                model_saver.save(sess, model_prefix, global_step=step)
+                print("Save leanred graph at step {}".format(step))
 
   
     def run(self):
--- v0.1/library/model/driver.py
+++ v0.1/library/model/driver.py
@@ -2,42 +2,45 @@
 
 import cnn as model1 
 import tensorflow as tf
+import make_input
 
 if __name__ == "__main__":
+
+    # Load input data
+    x, x_len, y = make_input.split_xy(csv_file_path="./input.csv", x_height=2, y_size=2)
+
 
     cnn = model1.CNN(directory="./")
 
     graph = tf.Graph()
     with graph.as_default():
         
-        cnn.restore(graph, model_name="CNN")
-        """
-        cnn.create_model(
-            data_file_path="./input.csv",
-            input_height = 2,
-            num_NN_nodes=[2,3], 
-            num_output=2, 
-            filter_sizes=[[2,2],[1,2]], 
-            num_filters=1)
-        """
-        
-        cnn.train(
-            dev_sample_percentage=0.1,
-            model_name="CNN",
-            batch_size=5,
-            num_epochs=3,
-            evaluate_every=100,
-            saver_every=100)
-    
+        # setting session 
+        session_conf = tf.ConfigProto(
+            allow_soft_placement=True,
+            log_device_placement=False)
 
-"""
-    session_conf = tf.ConfigProto(
-        allow_soft_placement=True,
-        log_device_placement=False)
-
-    with tf.Session(config=session_conf) as sess:
-        sess.run(tf.global_variables_initializer())
-        run_result = sess.run(cnn.test, feed_dict={cnn.input_x:[[[3,2],[5,4]], [[5,6],[8,9]]], cnn.input_y:[1,2]})
-
-    print (run_result)
-"""
+        with tf.Session(config=session_conf) as sess:
+             
+            cnn.restore(sess, graph, model_name="CNN")
+            """
+            cnn.create_model(
+                x_height = 2,
+                x_len = x_len,
+                num_NN_nodes=[2,3], 
+                y_size=2, 
+                filter_sizes=[[2,2],[1,2]], 
+                num_filters=1)
+            """      
+            cnn.train(
+                x = x,
+                y = y,
+                sess = sess,
+                dev_sample_percentage=0.1,
+                model_name="CNN",
+                batch_size=5,
+                num_epochs=3,
+                evaluate_every=100,
+                saver_every=100)
+            
+               
