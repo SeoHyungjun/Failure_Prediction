e76aff27615ce1f998f0bdaa854e5046dfde1105
Choi <superbag2010@naver.com>
2017-05-23 08:07:13 +0000

    update

--- v0.2/prophet/library/models.py
+++ v0.2/prophet/library/models.py
@@ -4,22 +4,6 @@ import sys
 import os
 import pandas as pd
 
-def load_mod(module_name) :
-#    cwd = os.getcwd()
-#    os.chdir(os.path.dirname(module_path))
-    # print(os.getcwd())
-    # print(os.path.basename(module_path))
-    module = __import__(module_name)
-    # try :
-    #     mod = __import__(os.path.basename(module_path))
-    # except ImportError as e:
-    #     print("ImportError :", os.path.abspath(__file__))
-    #     print(str(e))
-    #     print("Failed Load library. Check the MODEL_TBL.csv")
-    #     exit(0)
-
-#    os.chdir(cwd)
-    return module
 
 SOURCE_ROOT = os.path.join("/", "root", "Failure_Prediction", "v0.1")
 sys.path.insert(0, os.path.join(SOURCE_ROOT, "library", "data_transform"))
@@ -37,7 +21,7 @@ for index, row in pd_model_tbl.iterrows():
         module_dirpaths.append(module_dirpath)
         sys.path.insert(0, module_dirpath)
 
-    module = load_mod(module_name)
+    module = __import__(module_name)
     class_obj_dict[row['MODEL_NAME']] = getattr(module, row['CLASS_NAME'])
 
 print(class_obj_dict)
